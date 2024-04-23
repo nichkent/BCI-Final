27f482ec3f64786a2469392c2f73a78a8f777f36
Claire Leahy <cleahy@uvm.edu>
2024-04-23 19:06:16 -0400

    Make load data a function

--- load_data.py
+++ load_data.py
@@ -2,11 +2,12 @@
 """
 Created on Tue Apr 23 11:34:26 2024
 
-@author: nicho
+@authors: Arthur Dolimier, Nicholas Kent, Claire Leahy, and Aiden Pricer-Coan
 """
-#%% Load the data
 
-# Import
+#%% Import Packages
+import numpy as np
+from matplotlib import pyplot as plt
 import loadmat
 
 # Select subject data
@@ -16,18 +17,42 @@ subject_label = "k3b"
 # Find the file path
 data_file = f"data/{subject_label}.mat"
 
-# Load the data from the file
-data = loadmat.loadmat(data_file)
+#%% Load the data
 
-# Grab relevant fields from the data file
-s = data['s'] # EEG signal data array with potential NaN values indicating breaks or data saturation.
+"""
+    TODO:
+        - Prompt user for new input if file directory doesn't exist (if 'l1b', 'k6b', or 'k3b' aren't entered)?
+        - Look more into the classifications (1,2,3,4) and corresponding thoughts
+        - Docstrings
 
-fs = data['HDR']['SampleRate'] # Sample rate
-class_label = data['HDR']['Classlabel'] # Labels of each class (i.e. tongue, left hand, right hand, feet) and NAN which indicates the trials of the test set
-trig = data['HDR']['TRIG'] # Beginning of each trial
-artifact_selection = data['HDR']['ArtifactSelection'] # Indicates trials with artifacts which were visually identified
+"""
 
-# Debug
-#print(data['HDR']['Classlabel'])
-#print(data['HDR'].keys())
-print(data['s'])
+def load_eeg_data(subject):
+    
+    # Create a file name given subject input
+    data_file = f"data/{subject}.mat"
+
+    # Load the data from the file
+    data = loadmat.loadmat(data_file)
+    
+    # Extract relevant fields from the data file
+    signal = data['s'] # EEG signal data array with potential NaN values indicating breaks or data saturation
+    
+    fs = data['HDR']['SampleRate'] # Sampling rate
+    class_label = data['HDR']['Classlabel'] # Labels of each class (left hand=1, right hand=2, foot=3, tongue=4) and NAN (trials of the test set)
+    trigger_time = data['HDR']['TRIG'] # Start time of each trial
+    artifact_selection = data['HDR']['ArtifactSelection'] # Indicates trials with artifacts which were visually identified
+    
+    # Create a new dictionary with relevant fields
+    data_dictionary = {'Signal': signal,
+                       'Sampling Frequency': fs,
+                       'Class Label': class_label,
+                       'Start Times': trigger_time,
+                       'Artifact Trials': artifact_selection}
+    
+    # Debug
+    #print(data['HDR']['Classlabel'])
+    #print(data['HDR'].keys())
+    #print(data['s'])
+
+    return data_dictionary
---
+++ test_load_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 18:48:11 2024
+
+@authors: Arthur Dolimier, Nicholas Kent, Claire Leahy, and Aiden Pricer-Coan
+"""
+
+#%% Import packages
+from load_data import load_eeg_data
+
+#%% Load the data
+
+data_dictionary = load_eeg_data(subject='k3b')
