73fcdc67b2fce20a577ba908c3cbba7c5bd91f9f
Claire Leahy <cleahy@uvm.edu>
2024-04-28 21:47:26 -0400

    Multiline comments to guide filtering

--- clean_data.py
+++ clean_data.py
@@ -11,8 +11,7 @@ Sources:
 
 # Import packages
 import numpy as np
-from matplotlib import pyplot as plt
-from scipy.signal import firwin, filtfilt, freqz, hilbert
+from scipy.signal import firwin, filtfilt, hilbert
 
 #%% Considerations
 
@@ -27,9 +26,24 @@ Consider removing signals outside of 8-30Hz (SMR)
 """
 Filtering frequencies will help potentially eliminate noise and ideally clarify relevant EEG signals
 Bandpass frequencies can be changed to achieve greatest accuracies
+Would it make the most sense to filter then epoch or epoch then filter?
+How necessary is the envelope?
+"""
+
+"""
+Trials (epochs) where data_dictionary['Artifact Trials'] is 1 indicate an artifact has been identified
+MUCH less specific process than removing components
+Should ICA be performed on those trials (basically assuming electrodes=components) and then remove electrode source from that epoch? 
+60 channels present. Observing raw data at pertinent channels may help identify necessity of removing artifact (artifact may have minimal contribution to relevant channels)
+Is best way of approaching artifact removal looking at accuracy with and without artifact trials? Don't have a mixing matrix
 """
 
 #%% Make filter  
+
+"""
+Is bandpass the best type of filter for this case?
+Could make more general, decide on finite or infinite. Select parameters from there
+"""
   
 def make_bandpass_filter(low_cutoff, high_cutoff, filter_type='hann', filter_order=10, fs=250):
     
@@ -40,6 +54,12 @@ def make_bandpass_filter(low_cutoff, high_cutoff, filter_type='hann', filter_ord
     return filter_coefficients
 
 #%% Filter data
+
+"""
+Determine what data will be passed
+Epochs or all raw data? Could easily be applied to all and then run the epochs over it
+Take in a if infinite filter chosen
+"""
 
 def filter_data(data, b):
     
@@ -62,6 +82,11 @@ def filter_data(data, b):
 
 #%% Generate the envelope
 
+"""
+How useful would the envelope be? 
+Is this what we want to bootstrap, identify significance for classification?
+"""
+
 def get_envelope(filtered_data):
     
     # variables for sizing
@@ -75,6 +100,5 @@ def get_envelope(filtered_data):
     for channel_index in range(channel_count):
         
         envelope[channel_index]=np.abs(hilbert(x=filtered_data[channel_index]))
-        
 
-    return envelope  
+    return envelope
